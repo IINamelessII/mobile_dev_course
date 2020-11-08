b1adc64c01374d315316c74d98ab48b6e2916c95
Oleh Serikov <oserikov@jerasoft.net>
2020-11-09 01:08:54 +0200

    Tabs with spaces (again, but now in .js, lol)

--- mobile-app/components/Movies.js
+++ mobile-app/components/Movies.js
@@ -3,40 +3,40 @@ import {FlatList, SafeAreaView, StyleSheet} from 'react-native';
 import Movie from './Movie';
 
 const Movies = () => {
-	const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-	useEffect(() => {
-		(async () => {
-			let response = await fetch('https://8aebb2537381.ngrok.io/movies')
-			let responseData = await response.json();
-			setMovies(responseData.Search);
-		})();
-	}, []);
+  useEffect(() => {
+    (async () => {
+      let response = await fetch('https://8aebb2537381.ngrok.io/movies')
+      let responseData = await response.json();
+      setMovies(responseData.Search);
+    })();
+  }, []);
 
-	const renderMovie = movie => (
-		<Movie
-			title={movie.item['Title']}
-			year={movie.item['Year']}
-			type={movie.item['Type']}
-			poster={movie.item['Poster']}
-		/>
-	);
+  const renderMovie = movie => (
+    <Movie
+      title={movie.item['Title']}
+      year={movie.item['Year']}
+      type={movie.item['Type']}
+      poster={movie.item['Poster']}
+    />
+  );
 
-	return (
-		<SafeAreaView style={styles.container}>
-			<FlatList
-				data={movies}
-				renderItem={renderMovie}
-				keyExtractor={movie => movie['imdbID']}
-			/>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={movies}
+        renderItem={renderMovie}
+        keyExtractor={movie => movie['imdbID']}
+      />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		marginTop: 20,
-	},
+  container: {
+    marginTop: 20,
+  },
 });
 
 export default Movies;
